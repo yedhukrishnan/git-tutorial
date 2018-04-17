f8847bddc07371510a7fc66dcea81fa6729a6459
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2018-04-17 20:08:22 +0530

    Add miscellaneous mathematical functions

---
+++ misc.py
@@ -0,0 +1,11 @@
+# Miscellaneous math functions
+
+# Find the largest of two numbers
+def largest(a, b):
+    if a > b:
+        return a
+    return b
+
+
+print "Largest of 5 and 3: "
+print largest(5, 3)
