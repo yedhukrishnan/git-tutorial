918ed67764500bdef8d5c52ccf82b3cc470879ff
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2018-04-17 20:03:33 +0530

    Add function to find the sum of three numbers

--- add.py
+++ add.py
@@ -4,7 +4,12 @@ def add2(num1, num2):
     s = num1 + num2
     return s
 
+def add3(num1, num2, num3):
+    s = num1 + num2 + num3
+    return s
+
 a = 3
 b = 5
 
 print add2(a, b)
+print add3(3, 4, 5)
