b651a927b8a75b1318562af50709fd320a66abee
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2018-04-18 18:18:03 +0530

    Make a few styling changes

--- multiply.py
+++ multiply.py
@@ -3,12 +3,15 @@
 def multiply2(num1, num2):
     product = num1 * num2
     return product
+
 def multiply3(num1, num2,num3):
     product = num1 * num2 * num3
     return product
+
 def multiply4(num1, num2,num3,num4):
     product = num1 * num2 * num3 * num4
     return product
+
 print multiply2(2, 3)
 print multiply3(2, 3, 2)
 print multiply3(2, 3, 2,4)
