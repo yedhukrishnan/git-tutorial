e7c75f336655c7780014cba3e9aa05c6f17be4f8
YEDHIN KIZHAKKETHARA <18616@fisat>
2018-04-18 02:40:13 -0400

    Create function to multiply 3 numbers

--- multiply.py
+++ multiply.py
@@ -3,5 +3,9 @@
 def multiply2(num1, num2):
     product = num1 * num2
     return product
+def multiply3(num1, num2,num3):
+    product = num1 * num2 * num3
+    return product
 
 print multiply2(2, 3)
+print multiply3(2, 3, 2)
