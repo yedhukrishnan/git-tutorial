810aafcac3e1615cafa1c88c936d797c5c693d22
Neenu C M <neenucm1@gmail.com>
2018-04-18 06:14:34 -0400

    create a function to multiply

--- multiply.py
+++ multiply.py
@@ -6,6 +6,9 @@ def multiply2(num1, num2):
 def multiply3(num1, num2,num3):
     product = num1 * num2 * num3
     return product
-
+def multiply4(num1, num2,num3,num4):
+    product = num1 * num2 * num3 * num4
+    return product
 print multiply2(2, 3)
 print multiply3(2, 3, 2)
+print multiply3(2, 3, 2,4)
