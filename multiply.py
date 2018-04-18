051eeec94ff7914edd3ae15713b41140b6a7fd15
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2018-04-18 12:15:14 +0530

    Revert "Add function to multiply three numbers"
    This reverts commit c4223d99e57834626c45dbaf2ed02b62053e1a8d.

--- multiply.py
+++ multiply.py
@@ -4,10 +4,4 @@ def multiply2(num1, num2):
     product = num1 * num2
     return product
 
-def multiply3(num1, num2, num3):
-    product = num1 * num2 * num3
-    return product
-
 print multiply2(2, 3)
-print multiply3(4, 5, 6)
-
